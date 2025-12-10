9066d718986bacb5b111ef1fe3c741411f95c51d
inwf <inwffffff@outlook.com>
2025-12-10 15:54:26 +0800

    db

---
+++ db/db.sql
@@ -0,0 +1,10 @@
+create table user
+(
+    id   bigint auto_increment
+        primary key,
+    name varchar(255) null,
+    age  int          null
+)
+    charset = utf8mb3;
+
+INSERT INTO `canal-demo`.user (id, name, age) VALUES (1, 's 是 是', 11);
